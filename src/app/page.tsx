154b66ab36592323a9e6461fdbeecfbb91573511
ISHIMWE Ami Paradis <141851169+amiparadis250@users.noreply.github.com>
2024-05-26 23:13:12 +0200

    ch(redux): Configuring react redux
    - Set up Redux store for Next.js app
    - Modify _app.js to wrap app with Redux Provider
    - Pass Redux store instance to Provider
    - Update store configuration based on Redux Toolkit
    - Created welcome slice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+"use client"
+import { useAppSelector } from "@/redux/store";
+import Image from "next/image";
+
+
 export default function Home() {
+  const authState = useAppSelector((state) => state.auth.message);
+  console.log(authState);
+  console.log(".......................................................");
+  console.log("Testing Redux implementation...");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+<main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="bg-primaryBlue  rounded-md shadow-md hover:shadow-lg duration-200 cursor-pointer hover:scale-110">
         <p className="text-lg p-4">This is champs e commerce Homepage</p>
       </div>
     </main>
+    
   );
 }
